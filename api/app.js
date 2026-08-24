cf2d28b1224af15186b5b0076280b05d378ccb82
miloss98 <stojanovic98m@gmail.com>
2026-08-24 19:31:38 +0200

    Initialize React and Express monorepo

---
+++ api/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use("/feed", feedRoutes);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE",
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.listen(8080, console.log("server is running"));
---
+++ api/controllers/feed.js
@@ -0,0 +1,12 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({ title: "radi" });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { title: title, content: content },
+  });
+};
