ed251b0c527a0804580887bcf750dc12d1a28d30
miloss98 <stojanovic98m@gmail.com>
2026-08-24 20:09:29 +0200

    fix: fixed requests block with re-ordering the handlers

--- api/app.js
+++ api/app.js
@@ -6,7 +6,6 @@ const feedRoutes = require("./routes/feed");
 const app = express();
 
 app.use(bodyparser.json());
-app.use("/feed", feedRoutes);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,5 +16,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+
+app.use("/feed", feedRoutes);
 
 app.listen(8080, console.log("server is running"));
--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,5 +1,5 @@
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({ title: "radi" });
+  res.status(200).json([{ title: "radi" }]);
 };
 
 exports.createPost = (req, res, next) => {
