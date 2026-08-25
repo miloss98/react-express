e95b8b11cde3c2ab588a3973fb20ae974dbb4def
miloss98 <stojanovic98m@gmail.com>
2026-08-25 15:21:38 +0200

    chore: added new field to Post object, added console log to see successful requests from anywhere

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -5,8 +5,11 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  const imageUrl = req.body.imageUrl;
+  console.log("✅ POST /feed/create-post", "Data received:", req.body);
+
   res.status(201).json({
     message: "Post created successfully!",
-    post: { title: title, content: content },
+    post: { title: title, content: content, imageUrl: imageUrl },
   });
 };
