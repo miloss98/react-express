90a61cb9aab13e5b2fde568c4c1977ab161c0d32
miloss98 <stojanovic98m@gmail.com>
2026-08-26 18:14:23 +0200

    feat: added mongodb driver & mongoose, Post model added, mongoose connection established

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
@@ -19,4 +20,11 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080, console.log("server is running"));
+mongoose
+  .connect(
+    "mongodb+srv://stojanovic98m_db_user:APCPQ2PPN3ph0tZV@cluster0.sih3hyb.mongodb.net/?appName=Cluster0",
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,15 +1,41 @@
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json([{ title: "radi" }]);
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({ posts: posts });
+      console.log("✅ GET /feed/posts", posts);
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.body.imageUrl;
-  console.log("✅ POST /feed/create-post", "Data received:", req.body);
+  const creator = { name: "Milos" };
 
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: { title: title, content: content, imageUrl: imageUrl },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: imageUrl,
+    creator: creator,
   });
+
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        post: result,
+      });
+      console.log("✅ POST /feed/create-post", result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
