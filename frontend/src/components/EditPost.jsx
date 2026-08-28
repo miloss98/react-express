b1067156f9f1c99e10f99de6527e3a7b3c673354
miloss98 <stojanovic98m@gmail.com>
2026-08-28 18:12:54 +0200

    feat: added EditPost component as a modal to update the posts

---
+++ frontend/src/components/EditPost.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+
+export const EditPost = ({ postId, isOpen, onClose }) => {
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    imageUrl: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `http://localhost:8080/feed/update-post/${postId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      setFormData({
+        title: "",
+        content: "",
+        imageUrl: "",
+      });
+
+      onClose();
+    } catch (error) {
+      console.error("Error updating post:", error);
+    }
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+      <div className="w-[65vw] h-[35vw] bg-amber-200 flex flex-col gap-2 justify-center items-center">
+        <button
+          onClick={onClose}
+          className="text-3xl cursor-pointer text-red-500"
+        >
+          &times;
+        </button>
+
+        <p className="text-3xl font-semibold">Edit post</p>
+
+        <form
+          onSubmit={handleSubmit}
+          noValidate
+          className="gap-2 flex flex-col"
+        >
+          <div className="gap-2 flex">
+            <label htmlFor="title">Title</label>
+
+            <input
+              value={formData.title}
+              onChange={handleChange}
+              type="text"
+              name="title"
+              id="title"
+              className="border-black border-2"
+            />
+          </div>
+
+          <div className="gap-2 flex">
+            <label htmlFor="content">Content</label>
+
+            <input
+              value={formData.content}
+              onChange={handleChange}
+              type="text"
+              name="content"
+              id="content"
+              className="border-black border-2"
+            />
+          </div>
+
+          <div className="gap-2 flex">
+            <label htmlFor="imageUrl">Image URL</label>
+
+            <input
+              value={formData.imageUrl}
+              onChange={handleChange}
+              type="text"
+              name="imageUrl"
+              id="imageUrl"
+              className="border-black border-2"
+            />
+          </div>
+
+          <button type="submit" className="border border-black cursor-pointer">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,48 +1,79 @@
 import { useEffect, useState } from "react";
 import { AddPost } from "../components/AddPost";
+import { EditPost } from "../components/EditPost";
 
 export const Home = () => {
   const [posts, setPosts] = useState();
   const [loading, setLoading] = useState(true);
+  const [editingPost, setEditingPost] = useState(null);
+
+  const handleEdit = (post) => {
+    setEditingPost(post);
+  };
+
+  const handleCloseModal = () => {
+    setEditingPost(null);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("http://localhost:8080/feed/posts");
         const result = await response.json();
+
         setPosts(result.posts);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
+
     fetchData();
   }, []);
 
   if (loading) {
-    return <p> loading... </p>;
+    return <p>loading...</p>;
   }
 
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <h1 className="text-3xl py-4"> Feed </h1>
+      <h1 className="text-3xl py-4">Feed</h1>
+
       <div className="flex flex-col gap-6">
         {posts &&
           posts.map((post) => {
             return (
               <div
                 key={post._id}
-                className="flex flex-col gap-2 bg-gray-300 w-xl p-4"
+                className="flex flex-col gap-2 bg-gray-300 w-xl p-4 relative h-full"
               >
-                <p className="text-3xl font-bold"> {post.title}</p>
+                <p className="text-3xl font-bold">{post.title}</p>
+
                 <img src={post.imageUrl} width={200} height={200} alt="aaa" />
-                <a className="text-red-500" href={`/posts/${post._id}`}>
-                  See details &gt;
-                </a>
+
+                <div className="flex w-full justify-between">
+                  <a className="text-red-500" href={`/posts/${post._id}`}>
+                    See details
+                  </a>
+
+                  <button
+                    onClick={() => handleEdit(post)}
+                    className="text-blue-500 cursor-pointer"
+                  >
+                    Edit
+                  </button>
+                </div>
               </div>
             );
           })}
       </div>
+
+      <EditPost
+        post={editingPost}
+        isOpen={editingPost !== null}
+        onClose={handleCloseModal}
+      />
+
       <AddPost />
     </div>
   );
